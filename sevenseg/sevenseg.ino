567dd10914c22f2d5c891b682b30b80c37808c7c
wxv <wxv@users.noreply.github.com>
2016-09-14 22:39:47 -0400

    added looper to sevenseg

--- sevenseg/sevenseg.ino
+++ sevenseg/sevenseg.ino
@@ -9,7 +9,7 @@
 // Represents A, B, C, D, E, F, G, DP bars on display
 byte pins[8] = {7, 6, 5, 10, 11, 8, 9, 4};
 
-byte seg_table[16][8] =      // Display encodings
+byte seg_table[16][8] =      // Display encodings (DP unused)
   {{1, 1, 1, 1, 1, 1, 0, 0}, // 0
    {0, 1, 1, 0, 0, 0, 0, 0}, // 1
    {1, 1, 0, 1, 1, 0, 1, 0}, // 2
@@ -29,7 +29,7 @@ byte seg_table[16][8] =      // Display encodings
 
 void display_num(byte num)
 {
-  // Table 
+  // Display number based on pinout and display encoding table
   for (int i=0; i<8; i++)
   {
     digitalWrite(pins[i], seg_table[num][i]);
@@ -43,15 +43,34 @@ void setup()
   {
     pinMode(p, OUTPUT);
   }
-
 }
 
-void loop() 
+void cycle()
 {
   // Cycle through the numbers
   for (int i=0; i<16; i++)
   {
     display_num(i);
     delay(500);
+  }  
+}
+
+void looper()
+{
+  // Lights up segments in a fast clockwise loop
+  // Endless variations: switch direction, figure 8 loop, etc.
+  for (int i=0; i<6; i++)
+  {
+    for (int j=0; j<6; j++)
+    {
+      if (j==i) digitalWrite(pins[j], HIGH);
+      else digitalWrite(pins[j], LOW);
+    }
+    delay(50);
   }
+}
+void loop() 
+{
+  cycle();
+  //looper();
 }
