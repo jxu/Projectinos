c984a4fe890f7113ab5518a332e1cf1fb7438963
wxv <wxv@users.noreply.github.com>
2016-09-15 01:22:36 -0400

    added rainbow

---
+++ rainbow/rainbow.ino
@@ -0,0 +1,30 @@
+// Rainbow demo from RGB LED
+// Uses three out-of-phase sine waves, as described here: http://krazydad.com/tutorials/makecolors.php
+
+byte redpin = 9;
+byte greenpin = 10; 
+byte bluepin = 11;
+
+unsigned int STEPS = 256;
+unsigned int DELAY = 10;
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  for (int i=0; i<STEPS; i++)
+  { 
+    float red   = sin((i/(float)STEPS)*2*PI) * 127 + 128;
+    float green = sin((i/(float)STEPS)*2*PI + 2*PI/3) * 127 + 128;
+    float blue  = sin((i/(float)STEPS)*2*PI + 4*PI/3) * 127 + 128;
+
+    analogWrite(redpin, red);
+    analogWrite(greenpin, green);
+    analogWrite(bluepin, blue);
+
+    delay(DELAY);
+  }
+}
